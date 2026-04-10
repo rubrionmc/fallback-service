e59dd0ee879c91009f39c6e702f907965bd68007
Lennard <leycm@proton.me>
2026-04-10 06:10:08 +0200

    feat: add FallbackInstance and FallbackPodCommand classes Implement fallback server functionality with domain and port configuration. Includes handling of server list ping and client handshake packets.

---
+++ src/main/java/net/rubrion/server/fallback/FallbackInstance.java
@@ -0,0 +1,119 @@
+/*
+ * This file is part of the Rubrion Group.
+ *
+ * Licensed under the Rubrion Public License (RPL), Version 1, 2026.
+ * You may not use this file except in compliance with the License.
+ *
+ * License:
+ * https://rubrionmc.github.io/.github/licensens/RUBRION_PUBLIC_LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * Copyright (c) 2024 Rubrion Group. All rights reserved.
+ */
+package net.rubrion.server.fallback;
+
+import de.leycm.init4j.instance.Instanceable;
+import net.kyori.adventure.text.Component;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.server.ServerListPingEvent;
+import net.minestom.server.listener.manager.PacketListenerManager;
+import net.minestom.server.network.ConnectionState;
+import net.minestom.server.network.packet.client.handshake.ClientHandshakePacket;
+import net.minestom.server.network.player.PlayerConnection;
+import net.minestom.server.network.player.PlayerSocketConnection;
+import net.minestom.server.ping.Status;
+
+import lombok.NonNull;
+import lombok.Getter;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.lang.reflect.Field;
+
+@Getter
+public class FallbackInstance implements Instanceable {
+
+
+    public static @NonNull FallbackInstance getInstance() {
+        return Instanceable.getInstance(FallbackInstance.class);
+    }
+
+    private final @NonNull VarHandle protocolHandle;
+    private final @NonNull String domainSuffix;
+    private final int startPort;
+
+    @SuppressWarnings("UnstableApiUsage") // cause: we need this to set the protocol version for the connection
+    public FallbackInstance(final @NonNull String domainSuffix, final int startPort) {
+        this.domainSuffix = domainSuffix;
+        this.startPort = startPort;
+
+        try {
+            final Field field = PlayerSocketConnection.class.getDeclaredField("protocolVersion");
+            final MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(PlayerSocketConnection.class, MethodHandles.lookup());
+            protocolHandle = lookup.unreflectVarHandle(field);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError("Failed to init protocolVersion field");
+        }
+    }
+
+    private MinecraftServer server;
+    private PacketListenerManager packetHandler;
+    private GlobalEventHandler eventHandler;
+
+    @Override
+    public void onInstall() {
+        this.server = MinecraftServer.init();
+        this.packetHandler = MinecraftServer.getPacketListenerManager();
+        this.eventHandler = MinecraftServer.getGlobalEventHandler();
+
+        packetHandler.setListener(ConnectionState.HANDSHAKE, ClientHandshakePacket.class, this::handleHandshakePacket);
+        eventHandler.addListener(ServerListPingEvent.class, this::handleServerListPing);
+
+        server.start("0.0.0.0", startPort);
+    }
+
+    @Override
+    public void onUninstall() {
+        MinecraftServer.stopCleanly();
+    }
+
+    @SuppressWarnings("UnstableApiUsage") // cause: the protocol version is getting dropped by listening on the ClientHandshakePacket
+    private void handleHandshakePacket(
+            final @NonNull ClientHandshakePacket packet,
+            final @NonNull PlayerConnection connection
+    ) {
+        if (!(connection instanceof PlayerSocketConnection socketConnection)) return;
+
+        // note: hide from server scener by only accepting domain connections
+        if (!packet.serverAddress().endsWith(domainSuffix)) {
+            System.out.println("Rejecting connection from " + packet.serverAddress() + " because it does not end with " + domainSuffix);
+            try { socketConnection.getChannel().close(); }
+            catch (IOException _) { }
+            return;
+        }
+
+        // setting connection packet version to the players one for server side control
+        protocolHandle.set(socketConnection, packet.protocolVersion());
+
+        // note: 0 it a server ping handshake, witch is managed below
+        if (packet.intent().ordinal() == 0) return;
+
+        // kick player
+        connection.kick(Component.text("This server is a fallback server, and " +
+                "does not accept connections. Please connect to the main server instead."));
+        connection.disconnect();
+    }
+
+    private void handleServerListPing(final @NonNull ServerListPingEvent event) {
+        // note: catching ordinal 1
+        event.setStatus(Status.builder()
+                .description(Component.text("A Fallback Server"))
+                .build());
+    }
+}
---
+++ src/main/java/net/rubrion/server/fallback/FallbackPodCommand.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the Rubrion Group.
+ *
+ * Licensed under the Rubrion Public License (RPL), Version 1, 2026.
+ * You may not use this file except in compliance with the License.
+ *
+ * License:
+ * https://rubrionmc.github.io/.github/licensens/RUBRION_PUBLIC_LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * Copyright (c) 2024 Rubrion Group. All rights reserved.
+ */
+package net.rubrion.server.fallback;
+
+import de.leycm.init4j.instance.Instanceable;
+import picocli.CommandLine;
+
+// todo: add flag for --sun-misc-unsafe-memory-access=allow
+@CommandLine.Command(name = "rubrion fallback")
+public class FallbackPodCommand implements Runnable{
+
+    @SuppressWarnings("FieldMayBeFinal") // cause: this is a command arg
+    @CommandLine.Option(names = {"-d", "--domain"}, description = "the domain name of the server, e.g. rubrion.net")
+    private String domain = "host";
+
+    @SuppressWarnings("FieldMayBeFinal") // cause: this is a command arg
+    @CommandLine.Option(names = {"-p", "--port"}, description = "the port to open an Minecraft server socket on")
+    private int port = 8080;
+
+    private final FallbackInstance instance = new FallbackInstance(domain, port);
+
+    @Override
+    public void run() {
+        Instanceable.register(instance, FallbackInstance.class);
+    }
+
+    public void stop() {
+        Instanceable.unregister(instance.getClass());
+    }
+
+    static void main(final String[] args) {
+        final FallbackPodCommand instance = new FallbackPodCommand();
+        Runtime.getRuntime().addShutdownHook(new Thread(instance::stop, "shutdown"));
+        new CommandLine(instance).execute(args);
+    }
+}
